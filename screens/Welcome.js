9245f22f8da1f40c827e38175c1a7f9213be47b6
vijaykanth143 <kanthv413@gmail.com>
2022-06-10 11:59:29 +0530

    chat

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Text } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 const Welcome = ({ route }) => {
   const data = route.params;
   const profile = data.profiles.data;
-  return <Text>Welcome {profile.email}</Text>;
+  return <GiftedChat />;
 };
 
 export default Welcome;
